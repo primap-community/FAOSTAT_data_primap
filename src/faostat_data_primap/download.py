9d6fe031e157cc6538b8d5fc88cf97cf2b56deea
Daniel Busch <daniel.busch@climate-resource.com>
2024-11-05 09:48:13 +0100

    download script

---
+++ src/faostat_data_primap/download.py
@@ -0,0 +1,12 @@
+"""Downloads data from FAOSTAT website."""
+# links we will need:
+# download farm gate
+# https://bulks-faostat.fao.org/production/Emissions_crops_E_All_Data.zip
+# https://bulks-faostat.fao.org/production/Emissions_livestock_E_All_Data.zip
+# https://bulks-faostat.fao.org/production/Emissions_Agriculture_Energy_E_All_Data.zip
+# download land use and change
+# https://bulks-faostat.fao.org/production/Emissions_Land_Use_Forests_E_All_Data.zip
+# https://bulks-faostat.fao.org/production/Emissions_Land_Use_Fires_E_All_Data.zip
+# https://bulks-faostat.fao.org/production/Emissions_Drained_Organic_Soils_E_All_Data.zip
+# download pre and post agricultural production
+# https://bulks-faostat.fao.org/production/Emissions_Pre_Post_Production_E_All_Data.zip
