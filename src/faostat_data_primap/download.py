b2b6d72d5634fb7dcf1ddabf18619d4fe22be35a
Daniel Busch <daniel.busch@climate-resource.com>
2024-11-07 11:53:36 +0100

    basic script to download files [skip ci]

--- src/faostat_data_primap/download.py
+++ src/faostat_data_primap/download.py
@@ -1,113 +1,71 @@
 """Downloads data from FAOSTAT website."""
-# links we will need:
-# download farm gate
-# https://bulks-faostat.fao.org/production/Emissions_crops_E_All_Data.zip
-# https://bulks-faostat.fao.org/production/Emissions_livestock_E_All_Data.zip
-# https://bulks-faostat.fao.org/production/Emissions_Agriculture_Energy_E_All_Data.zip
-# download land use and change
-# https://bulks-faostat.fao.org/production/Emissions_Land_Use_Forests_E_All_Data.zip
-# https://bulks-faostat.fao.org/production/Emissions_Land_Use_Fires_E_All_Data.zip
-# https://bulks-faostat.fao.org/production/Emissions_Drained_Organic_Soils_E_All_Data.zip
-# download pre and post agricultural production
-# https://bulks-faostat.fao.org/production/Emissions_Pre_Post_Production_E_All_Data.zip
 
-import shutil
-import time
-import zipfile
+from datetime import datetime
 
-import requests
-from helper.definitions import downloaded_data_path, root_path
-from selenium.webdriver import Firefox
-from selenium.webdriver.firefox.options import Options
+import datalad.api
+from helper.definitions import downloaded_data_path
 
 if __name__ == "__main__":
-    # set options for headless mode
-    profile_path = ".firefox"
-    options = Options()
-    # options.add_argument('-headless')
+    sources = [
+        (
+            "farm_gate_emissions_crops",
+            "https://bulks-faostat.fao.org/production/Emissions_crops_E_All_Data.zip",
+        ),
+        (
+            "farm_gate_livestock",
+            "https://bulks-faostat.fao.org/production/Emissions_livestock_E_All_Data.zip",
+        ),
+        (
+            "farm_gate_agriculture_energy",
+            "https://bulks-faostat.fao.org/production/Emissions_Agriculture_Energy_E_All_Data.zip",
+        ),
+        (
+            "land_use_forests",
+            "https://bulks-faostat.fao.org/production/Emissions_Land_Use_Forests_E_All_Data.zip",
+        ),
+        (
+            "land_use_fires",
+            "https://bulks-faostat.fao.org/production/Emissions_Land_Use_Fires_E_All_Data.zip",
+        ),
+        (
+            "land_use_drained_organic_soils",
+            "https://bulks-faostat.fao.org/production/Emissions_Drained_Organic_Soils_E_All_Data.zip",
+        ),
+        (
+            "pre_post_agricultural_production",
+            "https://bulks-faostat.fao.org/production/Emissions_Pre_Post_Production_E_All_Data.zip",
+        ),
+    ]
+    for ds_name, url in sources:
+        # make downloaded_data folder if it doesn't exist yet
+        if not downloaded_data_path.exists():
+            downloaded_data_path.mkdir()
 
-    # create profile for headless mode and automatic downloading
-    options.set_preference("profile", profile_path)
-    options.set_preference("browser.download.folderList", 2)
+        # make data set folder if it doesn't exist yet
+        ds_path = downloaded_data_path / ds_name
+        if not ds_path.exists():
+            ds_path.mkdir()
 
-    # set up selenium driver
-    driver = Firefox(options=options)
-    # visit the main data page once to create cookies
-    driver.get("https://www.fao.org/faostat/en/#data")
+        # create unique directory
+        # TODO unique name to be discussed
+        today = datetime.today().strftime("%Y-%m-%d")
+        local_data_dir = ds_path / today
 
-    # wait a bit for the website to load before we get the cokkies
-    time.sleep(1)
+        if not local_data_dir.exists():
+            local_data_dir.mkdir()
 
-    # get the session id cookie
-    cookies_selenium = driver.get_cookies()
-    cookies = {}
-    for cookie in cookies_selenium:
-        cookies[cookie["name"]] = cookie["value"]
+        # download and commit with datalad
+        local_filename = local_data_dir / "test.zip"
+        datalad.api.download_url(
+            urls=url,
+            message=f"Added {ds_name}",
+            path=str(local_filename),
+        )
 
-    dataset = "emissions_crops"
-    url = "https://bulks-faostat.fao.org/production/Emissions_crops_E_All_Data.zip"
-
-    if not downloaded_data_path.exists():
-        downloaded_data_path.mkdir()
-
-    local_filename = downloaded_data_path / dataset / "insert_uniqie_identifier"
-
-    if not local_filename.parent.exists():
-        local_filename.parent.mkdir()
-
-    # if local_filename.exists() :
-    #     # check file size. if 210 or 212 bytes it's the error page
-    #     if Path(local_filename).stat().st_size in error_file_sizes :
-    #         # found the error page. delete file
-    #         os.remove(local_filename)
-
-    # now we have removed error pages, so a present file should not be overwritten
-    if (not local_filename.exists()) and (not local_filename.is_symlink()):
-        i = 0  # reset counter
-        while not local_filename.exists() and i < 10:  # noqa: PLR2004
-            # for i = 0 and i = 5 try to get a new session ID
-            if i in (1, 5):
-                driver = Firefox(options=options)
-
-                # visit the main data page once to create cookies
-                driver.get(url)
-                time.sleep(20)
-
-                # get the session id cookie
-                cookies_selenium = driver.get_cookies()
-                cookies = {}
-                for cookie in cookies_selenium:
-                    cookies[cookie["name"]] = cookie["value"]
-
-            r = requests.get(url, stream=True, cookies=cookies, timeout=120)
-            with open(str(local_filename), "wb") as f:
-                shutil.copyfileobj(r.raw, f)
-
-        if local_filename.exists():
-            print(f"Download => {local_filename.relative_to(root_path)}")
-            # unzip data (only for new downloads)
-            if local_filename.suffix == ".zip":
-                try:
-                    zipped_file = zipfile.ZipFile(str(local_filename), "r")
-                    zipped_file.extractall(str(local_filename.parent))
-                    print(f"Extracted {len(zipped_file.namelist())} files.")
-                    zipped_file.close()
-                # TODO Better error logging/visibilty
-                except zipfile.BadZipFile:
-                    print(
-                        f"Error while trying to extract "
-                        f"{local_filename.relative_to(root_path)}"
-                    )
-                except NotImplementedError:
-                    print(
-                        "Zip format not supported, " "please unzip on the command line."
-                    )
-            else:
-                print(
-                    f"Not attempting to extract "
-                    f"{local_filename.relative_to(root_path)}."
-                )
-        else:
-            print(f"Failed to download {local_filename.relative_to(root_path)}")
-
-    driver.close()
+        # Questions:
+        # * Push to datalad .zip and unzipped, or only unzipped?
+        # * What unique directory name to use -
+        # date or last updated from main data page?
+        # * Pydoit to execute download script that stages files
+        # and then push via command line
+        # or is there another solution?
