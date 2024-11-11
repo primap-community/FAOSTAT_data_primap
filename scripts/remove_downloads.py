8c000422352940486d213b5c2c4ccb472ef0d55a
Daniel Busch <daniel.busch@climate-resource.com>
2024-11-11 13:41:10 +0100

    remove zip files from github

--- scripts/remove_downloads.py
+++ scripts/remove_downloads.py
@@ -18,7 +18,7 @@ from faostat_data_primap.helper.definitions import downloaded_data_path
 @click.option(
     "--date",
     help="The day on which the data to be deleted was downloaded",
-    default="2024-11-11",
+    default="2023-11-09",
 )
 def run(date: str):
     """
