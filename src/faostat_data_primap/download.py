4d986ff6d07121d74ac71c124a1b9a3630d2e8e2
Daniel Busch <daniel.busch@climate-resource.com>
2024-11-12 10:57:31 +0100

    [DATALAD] Recorded changes

--- src/faostat_data_primap/download.py
+++ src/faostat_data_primap/download.py
@@ -8,10 +8,9 @@ from datetime import datetime
 import bs4
 import requests
 from bs4 import BeautifulSoup
+from exceptions import DateTagNotFoundError
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-
-from src.faostat_data_primap.exceptions import DateTagNotFoundError
 
 
 def get_html_content(url: str) -> bs4.BeautifulSoup:
